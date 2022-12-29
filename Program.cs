3c9125057cb57109c29385c4baa685d224dda0e5
polskiprogramistanet <rav@polskiprogramista.net>
2022-12-29 17:25:17 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClosuresExample_DynamicAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicAdd();
+        }
+
+        private static void DynamicAdd()
+        {
+            var m5 = GetMultiX(staticVal: 5);
+            var m10 = GetMultiX(staticVal: 10);
+
+            Console.WriteLine(m5(arg:10));
+            Console.WriteLine(m10(arg:10));
+        }
+
+        private static Func<int,int>GetMultiX(int staticVal)
+        {
+            return x => staticVal * x;
+        }
+    }
+}
